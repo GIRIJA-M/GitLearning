135bc059da4063050174a0017e00fd4eecffbf0f
Girija Mallavarapu <101454131+GIRIJA-M@users.noreply.github.com>
2026-05-09 20:44:00 +0530

    keep it simple

--- GitLearning/Program.cs
+++ GitLearning/Program.cs
@@ -1,7 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Girijaa!, one more modification");
-Console.WriteLine("Hello, Girijaa!, one more modification");
-Console.WriteLine("Hello, Girijaa!, one more modification");
-Console.WriteLine("Hello, Girijaa!, one more modification");
-Console.WriteLine("Hello, Girijaa!, one more modification");
+
 
